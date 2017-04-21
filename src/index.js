38534e921448c71421f8b3602a8775b12b315097
xuexb <fe.xiaowu@gmail.com>
2017-04-21 20:11:05 +0800

    add files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * @file MIP缓存删除
+ * @author xuexb <fe.xiaowu@gmail.com>
+ */
+
+import request from 'request';
+
+export default class MipCache {
+    constructor(options = {}) {
+        this.options = options;
+    }
+}
